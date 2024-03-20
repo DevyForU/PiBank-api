fd84332a6eb813205bc76468dba4295e214e4fb9
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-20 11:10:03 +0300

    feat: add user model

---
+++ src/main/java/com/devyforu/pibanks/Model/User.java
@@ -0,0 +1,17 @@
+package com.devyforu.pibanks.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Timestamp birthdayDate;
+    private Integer netMonthSalary;
+}
