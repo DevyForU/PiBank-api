f95f7b26314cc867665b7b434c3f3644cf8b5528
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-20 10:39:33 +0300

    feat: project gen

---
+++ src/main/java/com/devyforu/pibanks/PiBanksApplication.java
@@ -0,0 +1,13 @@
+package com.devyforu.pibanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiBanksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiBanksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devyforu/pibanks/PiBanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyforu.pibanks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiBanksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
