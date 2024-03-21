2d73a2d04bb1fbf9acd2dd935ae58a539025ca87
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-21 10:11:37 +0300

    feat: update attributes account

--- src/main/java/com/devyforu/pibanks/Model/Account.java
+++ src/main/java/com/devyforu/pibanks/Model/Account.java
@@ -1,15 +1,37 @@
 package com.devyforu.pibanks.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
+import java.util.List;
+
+
+
 @NoArgsConstructor
+@Data
 public class Account {
     private String id;
+    private User user;
+    private double mainBalance;
+    private double loans;
+    private double interestLoans;
     private double creditAllow;
-    private double overDraftLimit;
-    private String idUser;
+    private boolean overDraftLimit;
+    private List<Transaction> transactionList;
+
+    public Account(String id, double mainBalance, double loans, double interestLoans, double creditAllow, boolean overDraftLimit) {
+        this.id = id;
+        this.mainBalance = mainBalance;
+        this.loans = loans;
+        this.interestLoans = interestLoans;
+        this.creditAllow = creditAllow;
+        this.overDraftLimit = overDraftLimit;
+    }
+
+    public double getCreditAllow() {
+        return mainBalance / 3;
+    }
+    public boolean getOverDraftLimit(){
+        return false;
+    }
+
 }
