7dd337e8394cc9ea77fb3e8d6add2ac3968fbd73
Sun-2302 <hei.dera.932@gmail.com>
2024-03-21 18:48:44 +0300

    feat: add UserService

---
+++ src/main/java/com/devyforu/pibanks/Service/UserService.java
@@ -0,0 +1,36 @@
+package com.devyforu.pibanks.Service;
+
+import com.devyforu.pibanks.Model.User;
+import com.devyforu.pibanks.Repository.UserDAO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+    private UserDAO userDAO;
+
+    public List<User> findAll(){
+        return userDAO.findAll();
+    }
+
+    public User save(User toSave){
+        return userDAO.save(toSave);
+    }
+
+    public User delete(User toDelete){
+        return userDAO.delete(toDelete);
+    }
+
+    public User update(User toUpdate){
+        return userDAO.update(toUpdate);
+    }
+
+    public User getById(String id){
+        return userDAO.getById(id);
+    }
+
+
+}
