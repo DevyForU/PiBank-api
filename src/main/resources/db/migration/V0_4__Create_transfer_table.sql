b6fcc94ad86ff67bedb7041323ec293aa1fcdc9d
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-21 11:26:17 +0300

    feat: create table transfer

---
+++ src/main/resources/db/migration/V0_4__Create_transfer_table.sql
@@ -0,0 +1,8 @@
+create table if not exists "transfer_table"(
+    id varchar primary key default uuid_generate_v4(),
+    tranfer_reason varchar not null ,
+    amount double precision not null ,
+    effective_date date not null ,
+    registration_date date not null ,
+    is_canceled boolean default false
+)
