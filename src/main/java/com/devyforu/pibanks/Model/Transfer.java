8ec0943ca1b9ee1abc43e63ad1a06c62442aa542
Sun-2302 <hei.dera.932@gmail.com>
2024-03-21 20:19:44 +0300

    feat: add constructor to Transfer

--- src/main/java/com/devyforu/pibanks/Model/Transfer.java
+++ src/main/java/com/devyforu/pibanks/Model/Transfer.java
@@ -1,11 +1,12 @@
 package com.devyforu.pibanks.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Transfer {
