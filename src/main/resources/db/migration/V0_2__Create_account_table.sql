1d185ad0d298f3713f048741fed709282d0a9d1e
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-21 11:25:44 +0300

    feat: create table account

---
+++ src/main/resources/db/migration/V0_2__Create_account_table.sql
@@ -0,0 +1,6 @@
+create table if not exists "account_table"(
+    id varchar primary key  default uuid_generate_v4(),
+    id_user varchar references "user_table"(id) not null ,
+    credit_allow double precision not null,
+    over_draft_limit boolean default false
+)
