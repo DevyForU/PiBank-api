6d88b96b417e3e24cda671f63556cb4c363a1423
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-21 19:28:31 +0300

    feat: add new constructor

--- src/main/java/com/devyforu/pibanks/Model/User.java
+++ src/main/java/com/devyforu/pibanks/Model/User.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import java.sql.Timestamp;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class User {
     private String id;
@@ -15,4 +14,17 @@ public class User {
     private String lastName;
     private Timestamp birthdayDate;
     private double netMonthSalary;
+
+    public User(String id, String firstName, String lastName, Timestamp birthdayDate, double netMonthSalary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayDate = birthdayDate;
+        this.netMonthSalary = netMonthSalary;
+    }
+
+
+    public User(String id) {
+        this.id = id;
+    }
 }
