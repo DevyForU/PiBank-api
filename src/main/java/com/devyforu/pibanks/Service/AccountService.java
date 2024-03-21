3e4090d66a2c72b8ec73a0948968739fe4068365
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-21 10:12:30 +0300

    feat: add account service

---
+++ src/main/java/com/devyforu/pibanks/Service/AccountService.java
@@ -0,0 +1,44 @@
+package com.devyforu.pibanks.Service;
+
+import com.devyforu.pibanks.Model.Account;
+import com.devyforu.pibanks.Model.Transaction;
+import com.devyforu.pibanks.Model.TransactionType;
+import com.devyforu.pibanks.Model.Transfer;
+import com.devyforu.pibanks.Repository.AccountDAO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public void makeWithdrawal(String accountId, double amount) {
+        Account account = accountDAO.getById(accountId);
+        if (account != null) {
+            double availableBalance = account.getMainBalance() + account.getCreditAllow();
+            if (availableBalance >= amount) {
+                double newMainBalance = account.getMainBalance() - amount;
+                account.setMainBalance(newMainBalance);
+
+                accountDAO.update(account);
+
+                Transaction transaction = new Transaction();
+                transaction.setType(TransactionType.DEBIT);
+                transaction.setLabel("Withdrawal");
+                transaction.setTransfer(new Transfer(amount));
+
+                account.getTransactionList().add(transaction);
+
+                accountDAO.update(account);
+
+                System.out.println("Withdrawal successful.");
+            } else {
+                System.out.println("Error: Insufficient funds.");
+            }
+        } else {
+            System.out.println("Error: Account not found.");
+        }
+    }
+
+}
