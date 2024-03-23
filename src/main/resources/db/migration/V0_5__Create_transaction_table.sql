f920ef0e262311036da32f69abfd6729a4c8fb77
Sun-2302 <hei.dera.932@gmail.com>
2024-03-23 18:10:10 +0300

    feat: add id and type to the table transaction

--- src/main/resources/db/migration/V0_5__Create_transaction_table.sql
+++ src/main/resources/db/migration/V0_5__Create_transaction_table.sql
@@ -1,4 +1,6 @@
 create table if not exists "transaction"(
+    id varchar primary key default uuid_generate_v4(),
     id_transfer varchar references "transfer"(id) not null ,
-    label varchar
+    label varchar,
+    type varchar
 );
