715568fc769140e7c83fe5ab9c93af5917598f70
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-24 19:41:54 +0300

    fix: specify from which table is the attribut

--- src/main/resources/db/migration/V0_11_Create_function_getMainBalance.sql
+++ src/main/resources/db/migration/V0_11_Create_function_getMainBalance.sql
@@ -1,15 +1,16 @@
- 
-CREATE OR REPLACE FUNCTION getMainBalance(accountId varchar)
-RETURNS double precision AS
-$$
-DECLARE
-    mainBalance double precision;
-BEGIN
-    SELECT mainBalance INTO main_balance
-    FROM "balance"
-    WHERE idAccount = accountId;
 
-    RETURN mainBalance;
+CREATE OR REPLACE FUNCTION get_latest_balance(account_id VARCHAR)
+    RETURNS DOUBLE PRECISION AS $$
+DECLARE
+    latest_balance DOUBLE PRECISION;
+BEGIN
+    SELECT balance.mainBalance INTO latest_balance
+    FROM balance
+             INNER JOIN account ON balance.idAccount = account.id
+    WHERE balance.idAccount = account_id
+    ORDER BY balance.date DESC
+    LIMIT 1;
+
+    RETURN latest_balance;
 END;
-$$
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
