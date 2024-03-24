a804bcb426cbd449933d5b6b61c8b8fa156e894d
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2024-03-24 17:58:47 +0300

    refractor: change enum transaction_type to transactionType

--- src/main/resources/db/migration/V0_7__Create_table_transaction.sql
+++ src/main/resources/db/migration/V0_7__Create_table_transaction.sql
@@ -1,9 +1,9 @@
-CREATE TYPE transaction_type AS ENUM ('CREDIT', 'DEBIT');
+CREATE TYPE transactionType AS ENUM ('CREDIT', 'DEBIT');
 
 create table if not exists "transaction"(
     id varchar primary key default uuid_generate_v4(),
     idTransfer varchar references "transfer"(id) not null ,
     idAccount varchar references "account"(id) not null ,
     date timestamp without time zone default now(),
-    type transaction_type NOT NULL
+    type transactionType NOT NULL
 );
