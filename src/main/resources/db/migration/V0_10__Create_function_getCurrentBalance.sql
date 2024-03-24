0e8dd61dcdb3eb7f70aa8adade31416c695fce1c
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-24 18:26:20 +0300

    feat: create function getCurrentBalance

---
+++ src/main/resources/db/migration/V0_10__Create_function_getCurrentBalance.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION get_latest_balance(account_id VARCHAR)
+RETURNS TABLE (
+    id VARCHAR,
+    idAccount VARCHAR,
+    mainBalance DOUBLE PRECISION,
+    loans DOUBLE PRECISION,
+    loansInterest DOUBLE PRECISION,
+    date TIMESTAMP WITHOUT TIME ZONE
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT id, idAccount, mainBalance, loans, loansInterest, date
+    FROM balance
+    WHERE idAccount = account_id
+    ORDER BY date DESC
+    LIMIT 1;
+END;
+$$ LANGUAGE plpgsql;
--- src/main/resources/db/migration/V0_6__Create_table_transfer_list.sql
+++ src/main/resources/db/migration/V0_6__Create_table_transfer_list.sql
@@ -1,4 +1,4 @@
 create table if not exists "transferList"(
     idTransfer varchar references "transer"(id) not null ,
     idReceiver varchar references "account"(id)
-)
+);
