b12ddb010b89d6dcb5a73cfc024276201335094f
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-24 15:55:28 +0300

    feat: add POST, DELETE, GET /account and GET /account/{id}

--- src/main/java/com/devyforu/pibanks/Controller/Rest/AccountController.java
+++ src/main/java/com/devyforu/pibanks/Controller/Rest/AccountController.java
@@ -1,9 +1,12 @@
 package com.devyforu.pibanks.Controller.Rest;
 
+import com.devyforu.pibanks.Model.Account;
 import com.devyforu.pibanks.Service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -12,14 +15,26 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
     private AccountService service;
 
-    @PostMapping("/withdrawal")
-    public ResponseEntity<String> makeWithDrawal(@RequestParam String id,@RequestParam double amount){
-        try {
-            service.makeWithdrawal(id,amount);
-            return ResponseEntity.ok("Withdrawal successful.");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-
+    @GetMapping
+   public List<Account> findAll(){
+        return service.findAll();
     }
+    @PostMapping
+    public Account save(@RequestBody Account toSave){
+        return service.save(toSave);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id){
+        service.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Account getById(@PathVariable String id){
+        return service.getById(id);
+    }
+
+
+
 }
+
