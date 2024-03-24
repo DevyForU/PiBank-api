ba5d0235d091169c84364f49f01fc67d4407ee8e
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2024-03-24 17:56:05 +0300

    fix: change transaction type as enum

--- src/main/resources/db/migration/V0_7__Create_table_transaction.sql
+++ src/main/resources/db/migration/V0_7__Create_table_transaction.sql
@@ -1,7 +1,9 @@
+CREATE TYPE transaction_type AS ENUM ('CREDIT', 'DEBIT');
+
 create table if not exists "transaction"(
     id varchar primary key default uuid_generate_v4(),
     idTransfer varchar references "transfer"(id) not null ,
     idAccount varchar references "account"(id) not null ,
     date timestamp without time zone default now(),
-    type varchar not null check (type = 'CREDIT' or type = 'DEBIT')
+    type transaction_type NOT NULL
 );
