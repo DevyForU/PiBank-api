427a5f25be623e3a8af503b94e34a1601a0fc3a4
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-27 11:03:02 +0300

    chore: rename file in db/migration to conventional

--- src/main/resources/features/V8_Create_table_label.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists "label"(
-    id varchar primary key default uuid_generate_v4(),
-    name varchar not null unique ,
-    labelType transaction_type NOT NULL
-);
--- src/main/resources/features/V9__Create_table_labeling.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists "labeling"(
-    id_transaction varchar references "transaction"(id) not null ,
-    id_label varchar references "label"(id) not null
-) ;
