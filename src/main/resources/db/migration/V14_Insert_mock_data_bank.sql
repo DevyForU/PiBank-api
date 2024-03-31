3b6593105e017751000ba76314979c1d8f034cb7
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-31 21:14:26 +0300

    chore: add mock data

---
+++ src/main/resources/db/migration/V14_Insert_mock_data_bank.sql
@@ -0,0 +1,4 @@
+insert into "bank" (id, name, ref) values ('7b9beaed-9005-4c72-88f2-c4d0af992e0c', 'americanexpress', 'AXP');
+insert into "bank" (id, name, ref) values ('1250d813-a6a1-49c0-ac5d-7ecdad39f235', 'maestro', 'PAYC');
+insert into "bank" (id, name, ref) values ('9c4ed849-089b-4731-89e3-c53a4cc8bdff', 'mastercard', 'NEM');
+insert into "bank" (id, name, ref) values ('8a9d56e5-a410-4359-94b1-a064f43e3eb6', 'visa', 'JASN');
