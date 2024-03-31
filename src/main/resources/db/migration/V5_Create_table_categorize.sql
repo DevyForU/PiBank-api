87c6bbf69091cc209470c28403f5a849eaca3028
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-31 17:02:12 +0300

    fix: alter table name "categorize" to "category"

--- src/main/resources/db/migration/V5_Create_table_categorize.sql
+++ src/main/resources/db/migration/V5_Create_table_categorize.sql
@@ -1,5 +1,5 @@
 CREATE TYPE transaction_type AS ENUM ('CREDIT', 'DEBIT');
-create table if not exists "categorize"(
+create table if not exists "category"(
     id varchar primary key default uuid_generate_v4(),
     name varchar not null unique,
     type transaction_type not null
