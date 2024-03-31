3d9727faec93bb0bcd3258a45b7f91869e04f3bb
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-31 19:43:50 +0300

    refractor: rename table categorize to category

--- src/main/resources/db/migration/V11_Create_table_category.sql
+++ src/main/resources/db/migration/V11_Create_table_category.sql
@@ -1,4 +1,4 @@
-create table if not exists "categorize"(
+create table if not exists "category"(
     id varchar primary key default uuid_generate_v4(),
     name varchar not null unique,
     type transaction_type not null
--- src/main/resources/db/migration/V12_Update_table_transaction.sql
+++ src/main/resources/db/migration/V12_Update_table_transaction.sql
@@ -1,2 +1,2 @@
 alter table "transaction"
-add column id_categorize varchar references "categorize"(id) ;
+add column id_categorize varchar references "category"(id) ;
