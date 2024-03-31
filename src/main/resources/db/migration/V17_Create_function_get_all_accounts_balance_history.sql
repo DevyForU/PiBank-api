01ae4b7324ffc73349630125d40dda1876126925
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-31 21:31:57 +0300

    feat: create sql function get_all_accounts_balance_history

---
+++ src/main/resources/db/migration/V17_Create_function_get_all_accounts_balance_history.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION get_all_accounts_balance_history()
+RETURNS TABLE (
+    id VARCHAR,
+    id_account VARCHAR,
+    main_balance DOUBLE PRECISION,
+    loans DOUBLE PRECISION,
+    loans_interest DOUBLE PRECISION,
+    date TIMESTAMP
+) AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT
+        id,
+        id_account,
+        main_balance,
+        loans,
+        loans_interest,
+        date
+    FROM
+        balance_history
+    ORDER BY
+        id_account, date DESC;
+END;
+$$ LANGUAGE plpgsql;
