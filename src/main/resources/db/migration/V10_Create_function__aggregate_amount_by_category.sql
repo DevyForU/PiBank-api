eb0bdbcdaf5306f602f762931a12e169f667be0f
nirinaHelena <hei.nirina.7@gmail.com>
2024-04-01 21:38:00 +0300

    feat: create sql function aggregate_amount_by_category

---
+++ src/main/resources/db/migration/V10_Create_function__aggregate_amount_by_category.sql
@@ -0,0 +1,23 @@
+ 
+CREATE OR REPLACE FUNCTION aggregate_amount_by_category(start_date DATE, end_date DATE)
+RETURNS TABLE (
+    category_name VARCHAR,
+    total_amount DOUBLE PRECISION
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        c.name AS category_name,
+        SUM(tr.amount) AS total_amount
+    FROM
+        "transaction" t
+    JOIN
+        "transfer" tr ON t.id_transfer = tr.id
+    JOIN
+        "category" c ON t.id_categorize = c.id
+    WHERE
+        t.date >= start_date AND t.date < end_date
+    GROUP BY
+        c.name;
+END;
+$$ LANGUAGE plpgsql;
