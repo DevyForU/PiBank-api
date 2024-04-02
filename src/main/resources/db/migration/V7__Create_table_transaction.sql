8293f13d1564a9c40fae198ed396daec1d8997c5
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-04-02 18:50:06 +0300

    fix: add transaction type as enum

--- src/main/resources/db/migration/V7__Create_table_transaction.sql
+++ src/main/resources/db/migration/V7__Create_table_transaction.sql
@@ -1,4 +1,4 @@
-CREATE TYPE category_type AS ENUM ('DEBIT', 'CREDIT');
+CREATE TYPE transaction_type AS ENUM ('DEBIT', 'CREDIT');
 create table if not exists "transaction"(
     id varchar primary key default uuid_generate_v4(),
     id_transfer varchar references "transfer"(id) not null ,
